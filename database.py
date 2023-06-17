8abb2d935fc19ca4e5752bf2094e2c38e5657c3f
JustinSQiu <justinsunqiu@gmail.com>
2023-06-17 15:24:23 -0700

    temp

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 import sqlite3
 import pinecone
 
-def createMetadataTable():
+def initMetadataTable():
     # Connect to the SQLite database
     conn = sqlite3.connect('your_database.db')
     cursor = conn.cursor()
@@ -29,3 +29,22 @@ def createMetadataTable():
     # Commit the changes and close the connection
     conn.commit()
     conn.close()
+
+def initEmotionsTable():
+    pinecone.init(api_key="2d619f01-148b-4a3a-9bbf-4b8211f8409d", environment="test")
+    pinecone.create_index("hume-emotion", dimension=48)
+
+def insertEmotion(emotions):
+    index = pinecone.Index("hume-emotion")
+    for emotion in emotions:
+        index.upsert([
+            ("A", [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]),
+            ("B", [0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2]),
+            ("C", [0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3]),
+            ("D", [0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4, 0.4]),
+            ("E", [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5])
+        ])
+
+if __name__ == '__main__':
+    initMetadataTable()
+    initEmotionsTable()

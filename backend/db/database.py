92178c983596dad744772d24d305afd1093a7d3e
JustinSQiu <justinsunqiu@gmail.com>
2023-06-17 20:11:40 -0700

    database

--- backend/db/database.py
+++ backend/db/database.py
@@ -17,10 +17,51 @@ def initMetadataTable(cursor):
             timestamp INTEGER,
             start_time TEXT,
             end_time TEXT,
-            transcript_text TEXT,
+            transcript_text TEXT
             {emotion_columns} TEXT
         )
     ''')
+
+    user_id = "userhello"
+    entry_id = "entry456"
+    topic_id = "hello789"
+    sentence_id = "B"
+    video_link = "hello"
+    timestamp = 123
+    start_time = "00:00:10"
+    end_time = "00:00:20"
+    transcript_text = "This is a test sentence."
+    emotion_values = [0.1] * 48
+    cursor.execute("INSERT INTO Sentences (user_id, entry_id, topic_id, sentence_id, video_link, timestamp, start_time, end_time, transcript_text) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", 
+                   (user_id, entry_id, topic_id, sentence_id, video_link, timestamp, start_time, end_time, transcript_text))
+
+
+    user_id = "user123"
+    entry_id = "entry456"
+    topic_id = "hello789"
+    sentence_id = "A"
+    video_link = "hello"
+    timestamp = 123
+    start_time = "00:00:10"
+    end_time = "00:00:20"
+    transcript_text = "This is a test sentence."
+    emotion_values = [0.1] * 48
+    cursor.execute("INSERT INTO Sentences (user_id, entry_id, topic_id, sentence_id, video_link, timestamp, start_time, end_time, transcript_text) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", 
+                   (user_id, entry_id, topic_id, sentence_id, video_link, timestamp, start_time, end_time, transcript_text))
+
+
+    user_id = "user123"
+    entry_id = "entry456"
+    topic_id = "hellonot7990"
+    sentence_id = "C"
+    video_link = "hello"
+    timestamp = 123
+    start_time = "00:00:10"
+    end_time = "00:00:20"
+    transcript_text = "This is a test sentence."
+    emotion_values = [0.1] * 48
+    cursor.execute("INSERT INTO Sentences (user_id, entry_id, topic_id, sentence_id, video_link, timestamp, start_time, end_time, transcript_text) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", 
+                   (user_id, entry_id, topic_id, sentence_id, video_link, timestamp, start_time, end_time, transcript_text))
 
 def initEmotionsTable():
     pinecone.create_index("hume-emotion", dimension=48)
@@ -38,13 +79,45 @@ def getRelevantCommandIds(index, vector, top_k, cursor):
         top_k=top_k,
         include_values=True
     )
+    res = []
     for response in responses.matches:
         cursor.execute("SELECT * FROM Sentences WHERE sentence_id = ?", (response.id,))
         rows = cursor.fetchall()
-    return rows
+        if rows is not None and len(rows) != 0:
+            res.append({
+                "result": rows,
+                "score": response.score,
+            })
+    return res
 
-def rankExperiences(ids):
-    pass
+def rankExperiences(rows):
+    print(rows)
+    topicRanks = {}
+    for row in rows:
+        if row['result'][0][2] not in topicRanks:
+            topicRanks[row['result'][0][2]] = 1
+        else:
+            topicRanks[row['result'][0][2]] += 1
+    return sorted(topicRanks.items(), key=lambda x: x[0])
+
+
+def rank_topics(sentences, topic_sentences, top_k):
+    topic_ranks = {}  # Dictionary to store topic ranks
+
+    # Calculate aggregate rank for each topic
+    for topic, sentences_in_topic in topic_sentences.items():
+        aggregate_rank = sum(sentences[index] for index in sentences_in_topic)
+        topic_ranks[topic] = aggregate_rank
+
+    # Sort topics based on aggregate ranks
+    sorted_topics = sorted(topic_ranks.items(), key=lambda x: x[1], reverse=True)
+
+    # Retrieve the top-ranked topics
+    top_ranked_topics = [topic for topic, _ in sorted_topics[:top_k]]
+
+    return top_ranked_topics
+
+    # pass
 
 if __name__ == '__main__':
     conn = sqlite3.connect('your_database.db')
@@ -52,11 +125,12 @@ if __name__ == '__main__':
 
     pinecone.init(api_key="2d619f01-148b-4a3a-9bbf-4b8211f8409d", environment="asia-southeast1-gcp-free")
     index = pinecone.Index("hume-emotion")
-    # initMetadataTable(cursor)
+    initMetadataTable(cursor)
     # initEmotionsTable()
     # insertEmotion(index)
     query = [0.1] * 48
-    print(getRelevantCommandIds(index, query, 3, cursor))
+    rows = getRelevantCommandIds(index, query, 3, cursor)
+    print(rankExperiences(rows))
 
 
     # Commit the changes and close the connection
